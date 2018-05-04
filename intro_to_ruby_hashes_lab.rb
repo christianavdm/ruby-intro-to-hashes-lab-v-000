7b4dc6c33cf687fb7c78396031df9390f852c800
Christiana Vandermale <christianavdm@gmail.com>
2018-05-04 23:46:06 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def monopoly_with_third_tier
 	    :names => {
 	      :reading_railroad => {}, 
 	      :pennsylvania_railroad => {}, 
-	      
+	      :shortline => {}
 	    }, 
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25, 
